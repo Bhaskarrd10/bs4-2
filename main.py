d1ce4f9fb19076ae94140fe20d94032982f383e1
bhaskarreddy <bhaskrrd10@gmail.com>
2022-06-07 18:17:19 +0530

    beautiful soup

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
+#import lxml
 
 with open ("website.html") as file:
     contents = file.read()
 
-soup = BeautifulSoup(contents)
-print(contents)
+soup = BeautifulSoup(contents, "html.parser")
+#print(soup.prettify())
+#print(soup.a)
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    tag.get("href")
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
